5e8131e0ee20da9fb8d9254ffaebb30405947466
SyntraPXL25Kobe <Kobe.Bervoets@cursist.syntrapxl.be>
2026-06-09 00:19:56 +0200

    feat(components): update AppLogoIcon and AuthLayout usage

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,14 +1,16 @@
 import AppLogoIcon from '@/components/app-logo-icon';
+import { usePage } from '@inertiajs/react';
 
 export default function AppLogo() {
+    const { name } = usePage().props;
     return (
         <>
             <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
-                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
+                <AppLogoIcon className="size-5 text-white dark:text-black" />
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
                 <span className="mb-0.5 truncate leading-tight font-semibold">
-                    Laravel Starter Kit
+                    {name}
                 </span>
             </div>
         </>
