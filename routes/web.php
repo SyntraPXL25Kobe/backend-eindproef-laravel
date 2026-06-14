881ffbe59f8a46a018c721d3c9cdbb78575590fa
SyntraPXL25Kobe <Kobe.Bervoets@cursist.syntrapxl.be>
2026-06-14 16:04:56 +0200

    feat(routes): add route for pending coordinator registration

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'welcome')->name('home');
+Route::inertia('/register/coordinator/pending', 'auth/coordinator-pending')
+    ->name('register.coordinator.pending');
 
 Route::middleware(['auth', 'verified'])->prefix('app')->group(function () {
     Route::inertia('/', 'dashboard')->name('dashboard');
