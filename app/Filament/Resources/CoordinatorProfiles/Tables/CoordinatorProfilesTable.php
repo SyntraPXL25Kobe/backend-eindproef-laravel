5cd3a092ff2378980767ef023b2d02d193233c13
SyntraPXL25Kobe <Kobe.Bervoets@cursist.syntrapxl.be>
2026-06-17 17:11:18 +0200

    feat(coordinator-profiles): add CoordinatorProfilesTable class

---
+++ app/Filament/Resources/CoordinatorProfiles/Tables/CoordinatorProfilesTable.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Filament\Resources\CoordinatorProfiles\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\ViewAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class CoordinatorProfilesTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('user.name')
+                    ->searchable(),
+                TextColumn::make('organisation_name')
+                    ->searchable(),
+                TextColumn::make('vat_number')
+                    ->searchable(),
+                TextColumn::make('address')
+                    ->searchable(),
+                TextColumn::make('city')
+                    ->searchable(),
+                TextColumn::make('postal_code')
+                    ->searchable(),
+                TextColumn::make('country')
+                    ->searchable(),
+                TextColumn::make('website')
+                    ->searchable(),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                ViewAction::make(),
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
