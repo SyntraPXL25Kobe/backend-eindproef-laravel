b6fdebbefa6fd6769ebae77d8f6a0697c47c5a61
SyntraPXL25Kobe <Kobe.Bervoets@cursist.syntrapxl.be>
2026-06-17 17:10:25 +0200

    feat(coordinator-profiles): add CRUD pages for CoordinatorProfile

---
+++ app/Filament/Resources/CoordinatorProfiles/Pages/CreateCoordinatorProfile.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\CoordinatorProfiles\Pages;
+
+use App\Filament\Resources\CoordinatorProfiles\CoordinatorProfileResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateCoordinatorProfile extends CreateRecord
+{
+    protected static string $resource = CoordinatorProfileResource::class;
+}
