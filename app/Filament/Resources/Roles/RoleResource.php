6884b6298a0d07504ceffd851300c8778ec40e10
SyntraPXL25Kobe <Kobe.Bervoets@cursist.syntrapxl.be>
2026-06-17 17:17:26 +0200

    feat(roles): create RoleResource for role management

---
+++ app/Filament/Resources/Roles/RoleResource.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Filament\Resources\Roles;
+
+use App\Enums\Permission;
+use App\Filament\Resources\Roles\Pages\CreateRole;
+use App\Filament\Resources\Roles\Pages\EditRole;
+use App\Filament\Resources\Roles\Pages\ListRoles;
+use App\Filament\Resources\Roles\Pages\ViewRole;
+use App\Filament\Resources\Roles\Schemas\RoleForm;
+use App\Filament\Resources\Roles\Schemas\RoleInfolist;
+use App\Filament\Resources\Roles\Tables\RolesTable;
+use BackedEnum;
+use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Table;
+use Spatie\Permission\Models\Role;
+
+class RoleResource extends Resource
+{
+    protected static ?string $model = Role::class;
+
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedShieldCheck;
+
+    protected static ?string $navigationLabel = 'Rollen';
+
+    protected static string|\UnitEnum|null $navigationGroup = 'Beheer';
+
+    protected static ?int $navigationSort = 6;
+
+    public static function form(Schema $schema): Schema
+    {
+        return RoleForm::configure($schema);
+    }
+
+    public static function infolist(Schema $schema): Schema
+    {
+        return RoleInfolist::configure($schema);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return RolesTable::configure($table);
+    }
+
+    public static function getRelations(): array
+    {
+        return [];
+    }
+
+    public static function canViewAny(): bool
+    {
+        return auth()->user()?->can(Permission::ManageRoles->value) ?? false;
+    }
+
+    public static function canCreate(): bool
+    {
+        return auth()->user()?->can(Permission::ManageRoles->value) ?? false;
+    }
+
+    public static function canEdit($record): bool
+    {
+        return auth()->user()?->can(Permission::ManageRoles->value) ?? false;
+    }
+
+    public static function canDelete($record): bool
+    {
+        return auth()->user()?->can(Permission::ManageRoles->value) ?? false;
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ListRoles::route('/'),
+            'create' => CreateRole::route('/create'),
+            'view' => ViewRole::route('/{record}'),
+            'edit' => EditRole::route('/{record}/edit'),
+        ];
+    }
+}
