3190096dbc7d023152e892f6c15046c3179e4b82
SyntraPXL25Kobe <Kobe.Bervoets@cursist.syntrapxl.be>
2026-06-17 17:15:41 +0200

    feat(events): add EventForm and EventInfolist schemas

---
+++ app/Filament/Resources/Events/Schemas/EventForm.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Filament\Resources\Events\Schemas;
+
+use App\EventStatus;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+
+class EventForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                Select::make('coordinator_profile_id')
+                    ->relationship('coordinatorProfile', 'id')
+                    ->required(),
+                TextInput::make('title')
+                    ->required(),
+                Textarea::make('description')
+                    ->columnSpanFull(),
+                TextInput::make('location')
+                    ->required(),
+                DatePicker::make('start_date')
+                    ->required(),
+                DatePicker::make('end_date')
+                    ->required(),
+                Select::make('status')
+                    ->options(EventStatus::class)
+                    ->default('draft')
+                    ->required(),
+                TextInput::make('max_crew_members')
+                    ->numeric(),
+                FileUpload::make('cover_image_url')
+                    ->image(),
+            ]);
+    }
+}
